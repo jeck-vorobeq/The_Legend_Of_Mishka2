017f05a46e9b13a2a22da63aa36a62267b21d9e4
jeck-vorobeq <m.a.karasev2011@gmale.com>
2022-06-04 11:00:44 +0300

    lol

--- main.py
+++ main.py
@@ -1,2 +1,85 @@
+import pygame
+
+pygame.display.set_mode([800, 800])
+while True:
+
+    krestik=pygame.event.get()
+
+    for c in krestik:
+        if c.type==pygame.QUIT:
+            exit()
+        print(c)
 a="a"
 b="B"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ testo.py
@@ -0,0 +1,10 @@
+def paspakovcka(lol):
+    for c in lol:
+        print(c)
+paspakovcka([1])
+paspakovcka([])
+paspakovcka([1,2,3])
+paspakovcka(["lol"])
+paspakovcka([2])
+
+
