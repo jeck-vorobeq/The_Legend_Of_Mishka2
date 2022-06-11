1a42402ea049a2977891694426036094ad4eba03
jeck-vorobeq <m.a.karasev2011@gmale.com>
2022-06-11 13:46:42 +0300

    lol

--- controller.py
+++ controller.py
@@ -18,23 +18,33 @@ def krestik():
         if c.type==pygame.QUIT:
             exit()
     value=lol.get_hat(0)
+    lalue=lol.get_button(1)
+    walue = lol.get_button(0)
+    print(lalue)
     if value==(0,1):
         model.move_top()
-        lol.rumble(70,71,5)
+        lol.rumble(1,1,2)
         lol.stop_rumble()
     if value == (0, -1):
         model.move_bottom()
-        lol.rumble(70, 71, 5)
+        lol.rumble(1, 1, 2)
         lol.stop_rumble()
     if value == (1, 0):
         model.move_left()
-        lol.rumble(70, 71, 5)
+        lol.rumble(1, 1, 2)
         lol.stop_rumble()
     if value == (-1, 0):
         model.move_right()
-        lol.rumble(70, 71, 5)
+        lol.rumble(1, 1, 2)
         lol.stop_rumble()
-
+    if lalue == 1:
+        model.shield()
+        lol.rumble(50, 51, 1)
+        lol.stop_rumble()
+    if walue == 1:
+        model.sword()
+        lol.rumble(50, 51, 1)
+        lol.stop_rumble()
 
 
 
