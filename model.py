0d1ab20dbcdba3f5c36c1612067c0affd98230fb
jeck-vorobeq <m.a.karasev2011@gmale.com>
2022-07-16 10:39:30 +0300

    lol

--- model.py
+++ model.py
@@ -2,13 +2,15 @@ import pygame
 
 
 def move_top():
+    global state_hero
     hero.y -= 3
-
+    state_hero = "go_top"
 
 def move_bottom():
     global state_hero
     hero.y += 3
-    state_hero="go_bottom"
+    state_hero = "go_bottom"
+
 
 def move_right():
     global state_hero
@@ -25,7 +27,18 @@ def move_left():
 
 def state_hero_stand():
     global state_hero
-    state_hero = "stand"
+    if state_hero == "go_left":
+        state_hero = "stand_left"
+
+    if state_hero == "go_right":
+        state_hero = "stand_right"
+
+    if state_hero == "go_bottom":
+        state_hero = "stand_bottom"
+
+    if state_hero == "go_top":
+        state_hero = "stand_top"
+
 
 
 def sword():
@@ -34,6 +47,6 @@ def sword():
 
 floor = pygame.Rect(100, 100, 600, 600)
 hero = pygame.Rect(500, 500, 50, 60)
-state_hero = "stand"
+state_hero = "stand_left"
 
 a = 1
--- view.py
+++ view.py
@@ -3,6 +3,13 @@ import pygame, model, helper, time
 b = 2
 q = time.time()
 screen = pygame.display.set_mode([800, 800])
+top1 = pygame.image.load("hero/top1.png")
+top2 = pygame.image.load("hero/top2.png")
+top3 = pygame.image.load("hero/top3.png")
+top1 = pygame.transform.scale(top1, [154 / 2, 269 / 2])
+top2 = pygame.transform.scale(top2, [154 / 2, 269 / 2])
+top3 = pygame.transform.scale(top3, [154 / 2, 269 / 2])
+
 bottom1 = pygame.image.load("hero/bottom1.png")
 bottom2 = pygame.image.load("hero/bottom2.png")
 bottom3 = pygame.image.load("hero/bottom3.png")
@@ -31,12 +38,14 @@ def draw_hero():
         screen.blit(left2, model.hero)
     elif costum == "left3":
         screen.blit(left3, model.hero)
+
     if costum == "bottom1":
         screen.blit(bottom1, model.hero)
     elif costum == "bottom2":
         screen.blit(bottom2, model.hero)
     elif costum == "bottom3":
         screen.blit(bottom3, model.hero)
+
     if costum == "right1":
         screen.blit(right1, model.hero)
     elif costum == "right2":
@@ -44,18 +53,25 @@ def draw_hero():
     elif costum == "right3":
         screen.blit(right3, model.hero)
 
+    if costum == "top1":
+        screen.blit(top1, model.hero)
+    elif costum == "top2":
+        screen.blit(top2, model.hero)
+    elif costum == "top3":
+        screen.blit(top3, model.hero)
 
 def change_costum_hero():
     global costum
     if model.state_hero == "go_left":
 
-        if costum == "left1":
+        if costum != "left1" and costum != "left2":
+            costum = "left1"
+        elif costum == "left1":
             costum = "left2"
         elif costum == "left2":
             costum = "left3"
-        elif costum == "left3":
-            costum = "left1"
-
+    if model.state_hero == "stand_left":
+        costum = "left3"
     if model.state_hero == "go_right":
 
         if costum != "right1" and costum != "right2":
@@ -65,18 +81,28 @@ def change_costum_hero():
         elif costum == "right2":
             costum = "right3"
 
-    if model.state_hero == "stand":
+    if model.state_hero == "stand_right":
         costum = "right3"
 
-    if model.state_hero=="go_bottom":
+    if model.state_hero == "go_bottom":
         if costum != "bottom1" and costum != "bottom2":
             costum = "bottom1"
         elif costum == "bottom1":
             costum = "bottom2"
         elif costum == "bottom2":
             costum = "bottom3"
-        elif costum == "bottom3":
-            costum = "bottom1"
+    if model.state_hero == "stand_bottom":
+        costum = "bottom3"
+
+    if model.state_hero == "go_top":
+        if costum != "top1" and costum != "top2":
+            costum = "top1"
+        elif costum == "top1":
+            costum = "top2"
+        elif costum == "top2":
+            costum = "top3"
+    if model.state_hero == "stand_top":
+        costum = "top3"
 
 
 def play_animation_hero():
